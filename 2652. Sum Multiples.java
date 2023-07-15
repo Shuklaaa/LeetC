7a23ec636fe753469f9a27ec5da40a80f1dcd2c3
Shuklaaa <shuklaabhishek0404@gmail.com>
2023-07-15 21:25:58 +0530

    Using Array List

--- 2652. Sum Multiples.java
+++ 2652. Sum Multiples.java
@@ -17,3 +17,24 @@ class Solution {
         return count;
     }
 }
+
+
+// USING ARRAY LIST
+
+class Solution {
+    public int sumOfMultiples(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0 || i % 5 == 0 || i % 6 == 0 || i % 7 == 0){
+                list.add(i);
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < list.size(); i++) {
+            ans += list.get(i);
+        }
+
+        return ans;
+    }
+}
